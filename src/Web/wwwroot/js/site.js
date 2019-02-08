b51d8f721c1deca0c0f420e9d104af2bd2152f1a
raj <s531370@nwmissouri.edu>
2019-02-08 01:01:48 -0600

    added functionalty by adding javascript

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function bmi() {
+    var weight = document.getElementById("weight").value * 0.45;
+    var height = document.getElementById("heightFt").value * 12 * 0.025;
+    var bmi = weight / (height * height);
+    alert("Your BMi is " + Math.round(bmi * 10)/10);
+}
