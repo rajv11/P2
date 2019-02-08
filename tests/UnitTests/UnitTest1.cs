c1129bafe2329ea54a266b27b06b6f2687e62dc1
raj <s531370@nwmissouri.edu>
2019-02-08 15:31:40 -0600

    unit test

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -8,7 +8,27 @@ namespace UnitTests
         [Fact]
         public void Test1()
         {
+            int a = 2;
+            int expected = 2;
+            int actual = factorial(a);
+            Assert.Equal(expected,actual);
 
+        }
+        [Theory]
+        [InlineData(24,4)]
+        [InlineData(120,5)]
+        [InlineData(720,6)]
+        [InlineData(40320,8)]
+        public void TestAll(int exp,int a)
+        {
+            Assert.Equal(exp, factorial(a));
+        }
+        static int factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * factorial(n - 1);
         }
     }
 }
