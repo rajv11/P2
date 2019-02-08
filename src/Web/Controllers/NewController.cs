84374db52861ea7615315fa5bd4cf9e2aed4d8bd
raj <s531370@nwmissouri.edu>
2019-02-08 14:52:59 -0600

    added new controller and added methods for text, JSON, HTML and view

---
+++ src/Web/Controllers/NewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Text()
+        {
+            return Content("Some text");
+        }
+        public IActionResult Json()
+        {
+            return Json(new {
+                name = "New JSON data",
+                type = "JSON data"
+            });
+        }
+        public IActionResult Html()
+        {
+            return Content("<!DOCTYPE html><html><body><button> new buuton</button></body></html>", "text/html");
+        }
+    }
+}
